d4e52717419202410e2f61a1d8d857255b92b140
DanielCasadinho <d.casadinho@campus.fct.unl.pt>
2019-05-28 02:31:20 +0100

    Landscape Splines added

--- Source/Sintra/MyShapefileReader.cpp
+++ Source/Sintra/MyShapefileReader.cpp
@@ -61,30 +61,36 @@ TArray<FLine> UMyShapefileReader::getSplinePoints()
 	for (int i = 0; i < pLayer->GetFeatureCount(true); i++)
 	{
 		poFeature = pLayer->GetNextFeature();
-		OGRGeometry *poGeometry;
-		poGeometry = poFeature->GetGeometryRef();
-		if (poGeometry != NULL && wkbFlatten(poGeometry->getGeometryType()) == wkbLineString)
+    	GIntBig FID = poFeature->GetFID();
+		if (FID == 48 || FID == 83 || FID == 98 || FID == 100 || FID == 157 || FID == 101 || FID == 525 || FID == 558 || FID == 565 || FID == 1110
+			|| FID == 1125 || FID == 1126 || FID == 1127 || FID == 1137 || FID == 1167)
+//		if (FID == 44 || FID == 45 || FID == 46 || FID == 51 || FID == 145 || FID == 146 || FID == 147 || FID == 527 || FID == 528 || FID == 799
+//			|| FID == 800 || FID == 887 || FID == 888)
 		{
-			FLine line;
-			OGRLineString *poLineString = (OGRLineString *)poGeometry;
-			for (int k = 0; k < poLineString->getNumPoints(); k++)
+			OGRGeometry *poGeometry;
+			poGeometry = poFeature->GetGeometryRef();
+			if (poGeometry != NULL && wkbFlatten(poGeometry->getGeometryType()) == wkbLineString)
 			{
-				poLineString->getPoint(k, &point);
-				double xCoord = point.getX();
-				double yCoord = point.getY();
-			
+				FLine line;
+				OGRLineString *poLineString = (OGRLineString *)poGeometry;
+				for (int k = 0; k < poLineString->getNumPoints(); k++)
+				{
+					poLineString->getPoint(k, &point);
+					double xCoord = point.getX();
+					double yCoord = point.getY();
 
-				int col = (int) (xCoord - tifTransformFactor[0]) / tifTransformFactor[1];
-				int row = (int) (tifTransformFactor[3] - yCoord) / (-tifTransformFactor[5]);
 
-				tifBand->RasterIO(GF_Read, col, row, 1, 1, tifScanLine, 1, 1, GDT_Float32, 1, 0);
-			
-				FVector pointCoords = FVector((float)getWorldX(col), (float)getWorldY(row), (*tifScanLine)*100);
-				line.addPoint(pointCoords);
+					int col = (int)(xCoord - tifTransformFactor[0]) / tifTransformFactor[1];
+					int row = (int)(tifTransformFactor[3] - yCoord) / (-tifTransformFactor[5]);
+
+					tifBand->RasterIO(GF_Read, col, row, 1, 1, tifScanLine, 1, 1, GDT_Float32, 1, 0);
+
+					FVector pointCoords = FVector((float)getWorldX(col), (float)getWorldY(row), (*tifScanLine) * 100);
+					line.addPoint(pointCoords);
+				}
+				splinePoints.Add(line);
 			}
-			if(i<10) splinePoints.Add(line);
 		}
-		
 		OGRFeature::DestroyFeature(poFeature);
 	}
 	
